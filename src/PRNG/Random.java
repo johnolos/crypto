239403040c346c46512036fe200d18f4fea59e8e
John-Olav Storvold <johnolos@stud.ntnu.no>
2015-05-30 03:28:54 -0700

    Added some logic to quickly test the randomness of our algorithm

--- src/PRNG/Random.java
+++ src/PRNG/Random.java
@@ -1,34 +1,116 @@
 package PRNG;
 
 
+import java.util.concurrent.atomic.AtomicLong;
+
 public class Random {
 
-    private long time;
-    private long seed;
+    private AtomicLong seed;
+
+    private static final long multiplier = 0x5575L;
+    private static final long scrambleShift = 0x13L;
+    private static final long nextShift = 0x53F131L;
+    private static final long adder = 0x17L;
 
 
+    /**
+     * Creates a new Random generator. This constructor creates an instance of a
+     * Random generator to a value which is very unlikely to distinct from other
+     * instances created by the same constructor.
+     */
     public Random() {
         this(System.nanoTime());
     }
 
+    /**
+     * Creates a new Random generator based on the input seed given with it's
+     * parameter.
+     * @param seed Initial seed.
+     */
     public Random(long seed) {
-        this.time = System.currentTimeMillis();
-        this.seed = seed;
+        this.seed = new AtomicLong(scrambleSeed(seed));
+    }
+
+    private static long scrambleSeed(long seed) {
+        return (seed ^ multiplier) >> scrambleShift;
+    }
+
+    private long nextSeed() {
+        return (seed.get() * multiplier + adder) | nextShift;
     }
 
 
-    private double generate() {
-        return 0.0;
+    /**
+     * Generate a pseudo random precision number between 0.0 and 1.0.
+     * @return Returns a pseudo random double.
+     */
+    private int generate(int bits) {
+        long oldSeed, newSeed;
+        AtomicLong seed = this.seed;
+        do {
+            oldSeed = seed.get();
+            newSeed = nextSeed();
+        } while(!seed.compareAndSet(oldSeed, newSeed));
+        int number = (int)newSeed >>> (52 - bits);
+        int number2 = number % ((int)Math.pow(2,bits));
+        return number2;
     }
 
 
     /**
      * Produces a pseudo random number in the range between 0 and up to but not included n.
-     * @param n Number in range of [0,n)
-     * @return Random integer.
+     * @param n Upper bound of random number range.
+     * @return Returns a pseudo random number in range [0, n).
      */
-    public int randomInt(int n) {
-        return 0;
+    public int randomInt(int n){
+        assert n > 0;
+        return generate(n);
+    }
+
+    /**
+     * Produces a pseudo random integer.
+     * @return Returns a pseudo random.
+     */
+    public int randomInt() {
+        return generate(32);
+    }
+
+    public boolean randomBoolean() {
+        return generate(1) == 0;
+    }
+
+    /**
+     * Produces a pseudo random number in the range between base and up to but not included n.
+     * @param base Base number of the range to find random number.
+     * @param n Upper bound of random number range.
+     * @return Returns a pseudo random number in range [base, n).
+     */
+    public int randomInt(int base, int n) throws IllegalRangeException {
+        if(base < 0) {
+            throw new IllegalRangeException("Base was negative");
+        } else if(n <= base) {
+            throw new IllegalRangeException("Upper bound was lower or equal to base");
+        }
+
+        return (int)(generate(32)*(n-base) + base);
+    }
+
+
+    public void setSeed(long seed) {
+        this.seed.set(seed);
+    }
+
+
+    public class RandomException extends Exception {
+        public RandomException(String msg) {
+            super(msg);
+        }
+    }
+
+    public class IllegalRangeException extends RandomException {
+        public IllegalRangeException(String msg) {
+            super(msg);
+        }
     }
 
 
--- tests/PRNG/RandomTest.java
+++ tests/PRNG/RandomTest.java
@@ -2,6 +2,7 @@ package PRNG;
 
 import org.junit.Before;
 import org.junit.Test;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import static org.junit.Assert.*;
 
@@ -15,9 +16,37 @@ public class RandomTest {
         random = new Random();
         randomSeeded = new Random((System.nanoTime() % 1024) + System.currentTimeMillis());
     }
+    
 
     @Test
     public void testRandomInt() throws Exception {
+    }
+
+
+    @Test
+    public void testInt() throws Exception {
+
+        int numberOfIterations = 1000;
+        int numberOfBits = 4;
+        int expected = numberOfIterations / numberOfBits;
+        int offset = expected / numberOfBits;
+
+        int range = (int)Math.pow(2,numberOfBits);
+
+        int[] intArray = new int[range];
+        for(int i = 0; i < range; i++) {
+            intArray[i] = 0;
+        }
+
+        for(int i = 0; i < numberOfIterations; i++) {
+            int number = random.randomInt(numberOfBits);
+            intArray[number]++;
+        }
+
+        for(int i = 0; i < range; i++) {
+            int freq = intArray[i];
+            assertEquals(expected, freq, offset);
+        }
 
     }
 }
