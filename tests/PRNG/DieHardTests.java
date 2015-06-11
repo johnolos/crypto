b03f85b62d3cdd31e906c8db4922f941d63f6ee6
Johannes Lier <johannes.o.lier@gmail.com>
2015-06-11 00:28:05 -0700

    Runs test

--- tests/PRNG/DieHardTests.java
+++ tests/PRNG/DieHardTests.java
@@ -1,20 +1,20 @@
 package PRNG;
 
-import org.junit.Before;
-import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
 
 /**
  * Implementation of Diehard Tests to test randomness.
  */
 public class DieHardTests {
 
-    //private Random random;
-    java.util.Random random = new java.util.Random();
+    private Random random;
+//    java.util.Random random = new java.util.Random();
 
     class Point {
         int x, y;
@@ -23,49 +23,66 @@ public class DieHardTests {
             this.y = y;
         }
     }
-
+    
     @Before
     public void setUp() throws Exception {
-        random = new java.util.Random();
+//        random = new java.util.Random();
+    	random = new Random();
     }
-
+    
+    class tuple{
+    	int up;
+		int down;
+		tuple(int up, int down){
+			this.up = up;
+			this.down = down;
+		}
+    }
+    
     @Test
-    public void testBirthdaySpacing() throws Exception {
-        int n = (int)Math.pow(2, 24);
-        int m = (int)Math.pow(2, 10);
-
-        boolean[] birthdays = new boolean[n];
-
-        for(int i = 0; i < m; i++) {
-            int num = random.nextInt(n);
-            birthdays[num] = true;
-        }
-
-        List<Integer> listOfSpaceNumbers = new ArrayList<>();
-        int lastNumber = 0;
-        int currentNumber;
-        for(int i = 0; i < n; i++) {
-            if(birthdays[i]) {
-                currentNumber = i;
-                listOfSpaceNumbers.add(currentNumber - lastNumber);
-                lastNumber = currentNumber;
-            }
-        }
-
-        double sumOfSpaceNumbers = 0;
-        for(int i : listOfSpaceNumbers) {
-            sumOfSpaceNumbers+=i;
-        }
-
-        double mean = sumOfSpaceNumbers / listOfSpaceNumbers.size();
-
-        System.out.printf("Sum: %f.%n", sumOfSpaceNumbers);
-        System.out.printf("Mean of spacing: %f.%n", mean);
-        double expected = Math.pow(m, 3.0) / (4 * n);
-        assertEquals(expected, mean, 0.0);
+    public void testRuns(){
+    	ArrayList<Integer> sequence = new ArrayList<Integer>();
+    	
+    	int sequenceLength = 100000;
+    	
+    	for(int i = 0; i < sequenceLength; i++){
+    		sequence.add(random.randomInt());
+    	}
+    	
+    	tuple t = findRuns(sequence);
+    	float ratio = t.up/(float)t.down;
+    	System.out.println("UP: " + t.up + "\nDOWN: " + t.down + "\nRatio: " + ratio);
+    	assertEquals(1.0, ratio ,0.001);
     }
-
-
+    
+    public tuple findRuns(ArrayList<Integer> seq){
+    	int dir;
+    	int countUp = 0;
+    	int countDown = 0;
+		if(seq.get(1) > seq.get(0))
+			dir = -1;
+		else if(seq.get(1) < seq.get(0))
+			dir = 1;
+		else
+			dir = 0;
+		for(int i = 1; i < seq.size(); i++){
+			if(seq.get(i) > seq.get(i-1)){
+				if(dir == -1)
+					countUp++;
+				dir = 1;
+			}
+			else if(seq.get(i) < seq.get(i-1)){
+				if(dir == 1)
+					countDown++;
+				dir = -1;
+			}
+			else
+				dir = 0;
+		}
+		tuple t = new tuple(countUp,countDown);
+		return t;
+    }
+    
     @Test
     public void testMinimumDistance() throws Exception {
         int numberOfTests = 10;
@@ -89,8 +106,10 @@ public class DieHardTests {
         List<Point> points = new ArrayList<>();
         int x, y;
         for(int i = 0; i < n; i++) {
-            x = random.nextInt(l);
-            y = random.nextInt(l);
+//            x = random.nextInt(l);
+//            y = random.nextInt(l);
+        	x = random.randomInt(l);
+        	y = random.randomInt(l);
             points.add(new Point(x,y));
         }
 
